84d4d54a8eba265f17e252910ed24aa5978f2580
aln-f <144757715+aln-f@users.noreply.github.com>
2026-06-02 18:52:56 -0400

    Format code according to PEP8

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,11 +1,12 @@
-import requests, json
+import json
+import requests
 
 def sentiment_analyzer(text_to_analyse):
     url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
-    response = requests.post(url, json=myobj, headers=header)
-    if response.status_code == 200:        
+    response = requests.post(url, json=myobj, headers=header, timeout=1.5)
+    if response.status_code == 200:
         formatted_response = json.loads(response.text)
         label = formatted_response['documentSentiment']['label']
         score = formatted_response['documentSentiment']['score']
